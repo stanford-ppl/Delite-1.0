7b07e948b91e568904586d17ec93278630e37ca0
Anand Atreya <anand@atreya.us>
2011-01-25 17:08:39 -0800

    Removing 2.0 target app (moved to 2.0 repo)

--- apps/scala/src/ppl/apps/robotics/gradient2/Point2i.scala
+++
@@ -1,5 +0,0 @@
-package ppl.apps.robotics.gradient2
-
-class Point2i(val x: Int, val y: Int) {
-
-}
--- apps/scala/src/ppl/apps/robotics/gradient2/Rect.scala
+++
@@ -1,4 +0,0 @@
-package ppl.apps.robotics.gradient2
-
-class Rect(val x: Int, val y: Int, val width: Int, val height: Int) {
-}
